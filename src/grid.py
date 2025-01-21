8552cd2151e914629f700fa3b755ea95372ac6ba
Bart Koedijk <bartkoedijk@MacBook-Pro-van-Bart.local>
2025-01-21 14:19:40 +0100

    deleted updated_grid

--- src/grid.py
+++ src/grid.py
@@ -9,32 +9,57 @@ class Grid:
         self.lane_width = lane_width if lane_width % 2 == 0 else lane_width + 1
         print(self.lane_width)
         self.grid = np.zeros((size, size))
+        self.rotary_dict = []
+        self.flag = np.zeros((size, size), dtype=int)
         self.roads()
 
     def roads(self):
         """
         Add roads to the grid
         """
-        for i in range(0, self.size, self.blocks):
-            self.grid[max(0, i - self.lane_width + 1):i + 1, :] = 1
-            self.grid[:, max(0, i - self.lane_width + 1):i + 1] = 1
+        # for i in range(0, self.size, self.blocks):
+        #     self.grid[max(0, i - self.lane_width + 1):i + 1, :] = 1
+        #     self.grid[:, max(0, i - self.lane_width + 1):i + 1] = 1
         
-        # Add lanes on the edges
-        self.grid[:self.lane_width, :] = 1
-        self.grid[-self.lane_width:, :] = 1
-        self.grid[:, :self.lane_width] = 1
-        self.grid[:, -self.lane_width:] = 1
+        #Add lanes on the edges
+        # self.grid[:self.lane_width, :] = 2
+        # self.grid[-self.lane_width:, :] = 2
+        # self.grid[:, :self.lane_width] = 2
+        # self.grid[:, -self.lane_width:] = 2
 
+        for col in range(0, self.size, self.blocks):
+            left = col
+            right = min(col + self.lane_width, self.size)
+            for x in range(self.size):
+                for y in range(left, right):
+                    if self.grid[x, y] == 0:
+                        self.grid[x, y] = 1
         
-        # for i in range(self.blocks, self.size, self.blocks):
-        #     for j in range(self.blocks, self.size, self.blocks):
-        #         #2x2 rotary at intersection
-        #         x0 = max(0, i - 1)
-        #         y0 = max(0, j - 1)
-        #         x1 = min(self.size, i + 1)  # slicing end index is exclusive
-        #         y1 = min(self.size, j + 1)
-        #         # This creates a 2x2 block in [x0:x1, y0:y1]
-        #         self.grid[x0:x1, y0:y1] = 2
+        for row in range(0, self.size, self.blocks):
+            top = row
+            bottom = min(row + self.lane_width, self.size)
+            for x in range(top, bottom):
+                for y in range(self.size):
+                    if self.grid[x, y] == 0:
+                        self.grid[x, y] = 2
+                    # elif self.grid[x, y] == 1:
+                    #     self.grid[x, y] = 4
+
+
+        for i in range(self.blocks, self.size, self.blocks):
+            for j in range(self.blocks, self.size, self.blocks):
+                #2x2 rotary at intersection
+                x0 = i
+                x1 = i + self.lane_width
+                y0 = j
+                y1 = j + self.lane_width
+
+                self.grid[x0:x1, y0:y1] = 4
+
+                ring = [(x0,y0), (x0,y0+1), (x0+1,y0+1), (x0+1,y0)]
+                self.rotary_dict.append(ring)
+
+
 
     def plot(self):
         """
@@ -44,19 +69,12 @@ class Grid:
         plt.colorbar(label="Cell Type")
         plt.show()
 
-    def add_car(self, car):
-        """
-        Add a car to the grid
-        """
-        x, y = car
-        self.grid[x, y] = 3
 
 grid = Grid(25, 5, lane_width=2)
-grid.plot()
+
 cars = [
-    Car(grid, position=(0,0), direction='E'),
-    Car(grid, position=(0,10), direction='S'),
-    Car(grid, position=(10,0), direction='N')
+    Car(grid, position=(16,3), direction='E'),
+    Car(grid, position=(17,6), direction='N')
 ]
 
 
@@ -64,3 +82,4 @@ for _ in range(10):
     for car in cars:
         car.move_car()
         grid.plot()
+
